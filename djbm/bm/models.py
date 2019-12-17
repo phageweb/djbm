a74a14291ba88cfc65ea171f87c4570f31318bd5
honzan <novotny@elektroline.cz>
2019-12-17 06:34:53 +0100

    ReceivedData(models.Model)
    Signed-off-by: honzan <novotny@elektroline.cz>

--- djbm/bm/models.py
+++ djbm/bm/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 from django.utils import timezone
 
-# Field name Type   Field size AllowZeroLength  Default
-# Value ID  long integer, 
-# autonumber Section  
-# integer Table  
-# integer Round  
-# integer Board  
-# integer PairNS  
-# integer 
-# PairEW  
-# integer     
+# Field name Type   Field size AllowZeroLength  DefaultValue
+# ID  long integer, autonumber 
+# Section integer 
+# Table integer  
+# Round integer  
+# Board  integer 
+# PairNS  integer 
+# PairEW integer
 # Declarer integer 
 # NS/EW  text   2  yes 
 # Contract text   10  yes 
@@ -28,13 +26,26 @@ from django.utils import timezone
 
 class ReceivedData(models.Model):
     receivedData_id = models.BigIntegerField(primary_key=True)
-    NS_EW = models.CharField(max_length=2)
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+    Section = models.IntegerField() 
+    Table = models.IntegerField() 
+    Round = models.IntegerField() 
+    Board = models.IntegerField() 
+    PairNS = models.IntegerField() 
+    PairEW = models.IntegerField() 
+    Declarer = models.IntegerField() 
+    NS_EW       = models.CharField(max_length=2)
+    Contract    = models.CharField(max_length=10)
+    Result    = models.CharField(max_length=10)
+    LeadCard    = models.CharField(max_length=10)
+    Remarks    = models.CharField(max_length=255)
+    Processed = models.BooleanField(default=False)
+    Processed1 = models.BooleanField(default=False)
+    Processed2 = models.BooleanField(default=False)
+    Processed3 = models.BooleanField(default=False)
+    Processed4 = models.BooleanField(default=False)
+    Erased = models.BooleanField(default=False)
+    created_date = models.DateTimeField(default=timezone.now)
+
 
     def __str__(self):
-        return str(self.receivedData_id)
+        return str(self.receivedData_id+"_"+self.table+"_"+self.Board+"_"+self.PairNS+"_"+self.PairEW)
